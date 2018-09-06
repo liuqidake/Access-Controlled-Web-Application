f7cae42621a6805f511a24bfb99434e5abdb7b89
Qi Liu <qiliuchina@yahoo.com>
2018-09-05 19:30:21 -0600

    initialize app

---
+++ src/main/java/com/qiliu/springcloud/simplesocialmediaapplicationserver/SimpleSocialMediaApplicationServerApplication.java
@@ -0,0 +1,12 @@
+package com.qiliu.springcloud.simplesocialmediaapplicationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSocialMediaApplicationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSocialMediaApplicationServerApplication.class, args);
+	}
+}
