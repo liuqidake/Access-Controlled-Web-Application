0b5498cca1a4b4c0787a70cc3227daad28ac5bcf
liuqidake <qiliuchina@yahoo.com>
2018-09-06 21:04:58 -0600

    change controller name

--- src/main/java/com/qiliu/springcloud/simplesocialmediaapplicationserver/Controller/GreetingsController.java
+++ src/main/java/com/qiliu/springcloud/simplesocialmediaapplicationserver/Controller/GreetingsController.java
@@ -1,4 +1,4 @@
-package com.qiliu.springcloud.simplesocialmediaapplicationserver.controller;
+package com.qiliu.springcloud.simplesocialmediaapplicationserver.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/qiliu/springcloud/simplesocialmediaapplicationserver/source/RoleRepository.java
+++ src/main/java/com/qiliu/springcloud/simplesocialmediaapplicationserver/source/RoleRepository.java
@@ -1,5 +1,9 @@
 package com.qiliu.springcloud.simplesocialmediaapplicationserver.source;
 
-public class RoleRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.qiliu.springcloud.simplesocialmediaapplicationserver.Model.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
 
 }
